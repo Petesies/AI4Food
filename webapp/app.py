233617fea54e571bb4a2bee92dcc955fbe6e2e1d
Peter Johnston <peter.johnston1812@btinternet.com>
2024-03-17 20:24:49 +0000

    app.py updates

--- webapp/app.py
+++ webapp/app.py
@@ -4,13 +4,14 @@ import os
 import time
 import cv2
 from pycocotools import mask
+import numpy
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 # Initialize your mmdetection model
-config_file = 'C:/Users/peter/mmdetection/configs/AI4Food/configs/FoodConfig7.py'
-checkpoint_file = 'C:/Users/peter/mmdetection/work_dirs/FoodConfig7/epoch_12.pth'
+config_file = 'C:/Users/peter/mmdetection/configs/AI4Food/configs/foodseg103coco.py'
+checkpoint_file = 'C:/Users/peter/mmdetection/work_dirs/foodseg103coco/epoch_25.pth'
 model = init_detector(config_file, checkpoint_file)
 
 def allowed_file(filename):
@@ -43,17 +44,32 @@ def inference(filename):
 
 def inference_image(image_path):
     # Initialize the DetInferencer
+    model1 = init_detector(config_file, checkpoint_file)
+    pred = inference_detector(model, image_path)
     inferencer = DetInferencer(model=config_file, weights=checkpoint_file, palette=None)
 
     # Perform inference
     result = inferencer(image_path, out_dir='static/out/', show=False)
 
-    import struct
-    b = result['predictions'][0]['masks'][1]['counts'].encode('utf-8')
+    image_mask_np_uint8 = numpy.uint8(pred.pred_instances['masks'].cpu())  # Ensure uint8 format
+    image_mask_np_uint8 = numpy.squeeze(image_mask_np_uint8)
+
+    # Find contours in the mask
+    contours, _ = cv2.findContours(image_mask_np_uint8, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Assuming you want to work with the largest contour, you can sort contours by area
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # contours = cv2.findContours(pred.pred_instances)
+    print(cv2.contourArea(contours))
+    # print(cv2.contourArea(pred.pred_instances['masks'].cpu().numpy()))
+
+    # import struct
+    # b = result['predictions'][0]['masks'][1]['counts'].encode('utf-8')
 
     # value = struct.unpack('B', b)[0]
 
-    print("VALUE: ", b)
+    # print("VALUE: ", b)
 
     print("Result Dict Keys: ", list(result.keys()))
     print("Predictions Dict Keys: ", list(result['predictions'][0].keys()))
@@ -63,7 +79,7 @@ def inference_image(image_path):
     print("masks: ", result['predictions'][0]['masks'][1])
     print("masks keys: ", list(result['predictions'][0]['masks'][1].keys()))
     print("Counts type: ", type(result['predictions'][0]['masks'][0]['counts']))
-    print("Visualization list single list: ", [v for v in result['visualization'][0][5]])
+    # print("Visualization list single list: ", [v for v in result['visualization'][0][5]])
     print(len(result['predictions'][0]['masks']))
     # print([v for v in mask.decode(result['predictions'][0]['masks'][5])])
     # print("area: ", cv2.contourArea(mask.decode(result['predictions'][0]['masks'][5])))
