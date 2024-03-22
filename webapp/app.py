ce81885ab7ff6a41e0257488e276b3824aed9deb
Peter Johnston <peter.johnston1812@btinternet.com>
2024-03-22 18:00:44 +0000

    pixel to area

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ import cv2
 from pycocotools import mask
 import numpy
 from mmdet.registry import VISUALIZERS
-import mmcv
+from math import pi
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
@@ -15,6 +15,7 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 config_file = 'C:/Users/peter/mmdetection/configs/AI4Food/configs/foodseg103coco.py'
 checkpoint_file = 'C:/Users/peter/mmdetection/work_dirs/foodseg103coco/epoch_25.pth'
 model = init_detector(config_file, checkpoint_file)
+DIAMETER = 10
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
@@ -83,9 +84,18 @@ def inference_image(image_path):
         contours, _ = cv2.findContours(pred_masks[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         temp = [cv2.contourArea(contours[0]), pred_dict['scores'][i], pred_dict['labels'][i] + 1]
         arealist.append(temp)
-        print("Area: ", cv2.contourArea(contours[0]))
-        print("Score: ", pred_dict['scores'][i])
-        print("Label Number: ", pred_dict['labels'][i] + 1, "\n\n")
+        # print("Area: ", cv2.contourArea(contours[0]))
+        # print("Score: ", pred_dict['scores'][i])
+        # print("Label Number: ", pred_dict['labels'][i] + 1, "\n\n")
+
+    ratio = pixel_to_area(arealist)
+
+    print(ratio)
+
+    for i in arealist:
+        tempArea = i[0] * ratio
+        i.append(tempArea)
+        print(i)
 
     return arealist
 
@@ -95,6 +105,18 @@ def pred_to_dict(pred):
     pred_scores = pred.pred_instances.scores.clone().detach()
     pred_labels = pred.pred_instances.labels.clone().detach()
     return {"masks": pred_masks.cpu().numpy(), "scores": pred_scores.cpu().numpy(), "labels": pred_labels.cpu().numpy()}
+
+def pixel_to_area(maskList):
+    sticker_area = (DIAMETER / 2)**2 * pi
+    pixel_to_area_ratio = 0
+    for i in maskList:
+        if i[2] == 8:
+            pixel_to_area_ratio = sticker_area / i[0]
+
+    return pixel_to_area_ratio
+
+def area_to_cal(maskList):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
