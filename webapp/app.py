24d01bd146338ad9d456eca4940ffc17b37bb802
Peter Johnston <peter.johnston1812@btinternet.com>
2024-03-24 14:42:37 +0000

    sticker calibration code - done

--- webapp/app.py
+++ webapp/app.py
@@ -14,8 +14,10 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 # Initialize your mmdetection model
 config_file = 'C:/Users/peter/mmdetection/configs/AI4Food/configs/foodseg103coco.py'
 checkpoint_file = 'C:/Users/peter/mmdetection/work_dirs/foodseg103coco/epoch_25.pth'
-model = init_detector(config_file, checkpoint_file)
-DIAMETER = 10
+sticker_config = 'C:/Users/peter/mmdetection/configs/AI4Food/configs/stickerPre.py'
+sticker_checkpoint = 'C:/Users/peter/mmdetection/work_dirs/stickerPre/epoch_4.pth'
+# model = init_detector(config_file, checkpoint_file)
+DIAMETER = 1.8
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
@@ -48,13 +50,35 @@ def inference(filename):
 def inference_image(image_path):
     # Initialize the DetInferencer
     model1 = init_detector(config_file, checkpoint_file)
-    pred = inference_detector(model, image_path)
-    inferencer = DetInferencer(model=config_file, weights=checkpoint_file, palette=None)
+    pred = inference_detector(model1, image_path)
 
-    # Perform inference
+    inferencer = DetInferencer(model=config_file, weights=checkpoint_file, palette=None)
     result = inferencer(image_path, out_dir='static/out/', show=False)
 
-    pred_dict = pred_to_dict(pred)
+    pred_masks, pred_dict = pred_to_dict(pred)
+
+    arealist = []
+
+    for i in range(len(pred_masks)):
+        contours, _ = cv2.findContours(pred_masks[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        temp = [cv2.contourArea(contours[0]), pred_dict['scores'][i], pred_dict['labels'][i] + 1]
+        arealist.append(temp)
+
+    ratio = pixel_to_area(image_path)
+
+    for i in arealist:
+        tempArea = i[0] * ratio
+        i.append(tempArea)
+        # print(i)
+
+    return arealist
+
+# Code form sizing_module.py provided by Chris Moorhead, modfied for numpy output
+def pred_to_dict(pred):
+    pred_masks = pred.pred_instances.masks.clone().detach()
+    pred_scores = pred.pred_instances.scores.clone().detach()
+    pred_labels = pred.pred_instances.labels.clone().detach()
+    pred_dict = {"masks": pred_masks.cpu().numpy(), "scores": pred_scores.cpu().numpy(), "labels": pred_labels.cpu().numpy()}
 
     pred_masks = []
     for i in pred_dict['masks']:
@@ -75,43 +99,25 @@ def inference_image(image_path):
 
     pred_masks = numpy.array(pred_masks)
 
-    arealist = []
+    return pred_masks, pred_dict
 
-    print(len(pred_dict['scores']))
-    print(len(pred_dict['labels']))
-    print(len(pred_masks))
-    for i in range(len(pred_masks)):
-        contours, _ = cv2.findContours(pred_masks[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        temp = [cv2.contourArea(contours[0]), pred_dict['scores'][i], pred_dict['labels'][i] + 1]
-        arealist.append(temp)
-        # print("Area: ", cv2.contourArea(contours[0]))
-        # print("Score: ", pred_dict['scores'][i])
-        # print("Label Number: ", pred_dict['labels'][i] + 1, "\n\n")
-
-    ratio = pixel_to_area(arealist)
-
-    print(ratio)
-
-    for i in arealist:
-        tempArea = i[0] * ratio
-        i.append(tempArea)
-        print(i)
-
-    return arealist
-
-# Code form sizing_module.py provided by Chris Moorhead, modfied for numpy output
-def pred_to_dict(pred):
-    pred_masks = pred.pred_instances.masks.clone().detach()
-    pred_scores = pred.pred_instances.scores.clone().detach()
-    pred_labels = pred.pred_instances.labels.clone().detach()
-    return {"masks": pred_masks.cpu().numpy(), "scores": pred_scores.cpu().numpy(), "labels": pred_labels.cpu().numpy()}
-
-def pixel_to_area(maskList):
+def pixel_to_area(img_path):
+    model2 = init_detector(sticker_config, sticker_checkpoint)
+    pred_sticker = inference_detector(model2, img_path)
+    pred_mask_sticker, pred_dict_sticker = pred_to_dict(pred_sticker)
     sticker_area = (DIAMETER / 2)**2 * pi
     pixel_to_area_ratio = 0
-    for i in maskList:
-        if i[2] == 8:
-            pixel_to_area_ratio = sticker_area / i[0]
+    arealistSticker = []
+
+    for i in range(len(pred_mask_sticker)):
+        contours, _ = cv2.findContours(pred_mask_sticker[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        temp = [cv2.contourArea(contours[0]), pred_dict_sticker['scores'][i], pred_dict_sticker['labels'][i]]
+        arealistSticker.append(temp)
+
+    max_index = arealistSticker[1].index(max(arealistSticker[1]))
+
+    if arealistSticker[max_index][2] == 0:
+        pixel_to_area_ratio = sticker_area / arealistSticker[0][max_index]
 
     return pixel_to_area_ratio
 
